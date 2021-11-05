1f98537dec596ea08e9db621fb878b3e676367fe
Sam Miller <sam@itsecureadmin.com>
2021-11-05 16:22:47 -0700

    add navbar and product components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+      NavbarComponent,
+      ProductComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
