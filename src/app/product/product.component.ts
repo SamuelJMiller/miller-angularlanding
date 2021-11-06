4462ed221cedf468a11279f262751e51d98c52e2
Sam Miller <sam@itsecureadmin.com>
2021-11-05 21:44:52 -0700

    basic requirements

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  products: any[] = [
+    { name: 'Model A', desc: 'This is the smallest of the three pianos measuring six feet two inches long.', imagepath: 'assets/images/model-a.jpg' },
+    { name: 'Model B', desc: 'This is the next size up, sizing in at six foot eleven.', imagepath: 'assets/images/model-b.jpg' },
+    { name: 'Model D', desc: 'This is the Concert Model, coming in at a massive eight feet and eleven inches to deliver a stunning sound in even the largest of venues.',
+      imagepath: 'assets/images/model-d.jpg' }
+  ];
+
   constructor() { }
 
   ngOnInit() {
